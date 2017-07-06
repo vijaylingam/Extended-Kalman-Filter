b399d8a580b4b09f970a665984e5a5616bfcea1a
vijaylingam <vijay.lingam@ashoka.edu.in>
2017-07-06 19:49:20 +0530

    Extended Kalman Filter

---
+++ src/tools.hpp
@@ -0,0 +1,22 @@
+#ifndef TOOLS_HPP
+#define TOOLS_HPP
+
+#include <vector>
+#include "Eigen/Dense"
+
+namespace tools {
+
+    /**
+    * A helper method to calculate RMSE.
+    */
+    Eigen::VectorXd
+    CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, const std::vector<Eigen::VectorXd> &ground_truth);
+
+    /**
+    * A helper method to calculate Jacobians.
+    */
+    Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd &x_state);
+
+};
+
+#endif /* TOOLS_HPP */
